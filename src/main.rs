174dddc8134136e4c81e148e683c5f3bc68f157d
aichingert <aichinger.tobias13@gmail.com>
2024-02-02 18:36:48 +0100

    feat: added visualization of the data

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-struct MyCamera;
+struct MyCamera {
+    x: f32,
+    y: f32,
+    z: f32,
+}
 
 fn main() {
     App::new()
@@ -16,32 +20,37 @@ fn move_camera(
     keys: Res<Input<KeyCode>>,
     mut camera: Query<(&mut MyCamera, &mut Transform)>,
 ) {
-    let (_, mut transform) = camera.get_single_mut().unwrap();
+    let (mut values, mut transform) = camera.get_single_mut().unwrap();
 
     if keys.pressed(KeyCode::W) {
-        transform.translation.y += 1.;
+        values.x += 0.001;
+        transform.rotate_around(Vec3::ZERO, Quat::from_rotation_x(values.x));
     }
     if keys.pressed(KeyCode::S) {
-        transform.translation.y -= 1.;
+        values.x -= 0.001;
+        transform.rotate_around(Vec3::ZERO, Quat::from_rotation_x(values.x));
     }
+
     if keys.pressed(KeyCode::A) {
-        transform.translation.x -= 1.;
+        values.y += 0.001;
+        transform.rotate_around(Vec3::ZERO, Quat::from_rotation_y(values.y));
     } 
     if keys.pressed(KeyCode::D) {
-        transform.translation.x += 1.;
+        values.y -= 0.001;
+        transform.rotate_around(Vec3::ZERO, Quat::from_rotation_y(values.y));
     }
 }
 
 fn spawn_camera(
     mut commands: Commands,
-) {
+) { 
     commands.spawn((
         Camera3dBundle {
-            transform: Transform::from_xyz(10., 10., 10.)
+            transform: Transform::from_xyz(10., 10., 20.)
                 .looking_at(Vec3::ZERO, Vec3::Y),
                 ..default()
         },
-        MyCamera,
+        MyCamera { x: 0., y: 0., z: 0. },
     ));
 }
 
@@ -49,14 +58,28 @@ fn spawn_cube(
     mut commands: Commands,
     mut mesh: ResMut<Assets<Mesh>>,
 ) {
-
     let shape = mesh.add(shape::Cube::default().into());
 
-    commands.spawn((
-        PbrBundle {
-            mesh: shape,
-            transform: Transform::from_xyz(0., 0., 0.),
-            ..default()
-        },
-    ));
+    let data = std::fs::read_to_string("data").unwrap();
+
+    for line in data.lines() {
+        let (src, dst) = line.split_once("~").unwrap();
+
+        let src_xyz = src.split(',').map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>();
+        let dst_xyz = dst.split(',').map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>();
+
+        for x in src_xyz[0]..=dst_xyz[0] {
+            for y in src_xyz[1]..=dst_xyz[1] {
+                for z in src_xyz[2]..=dst_xyz[2] {
+                    commands.spawn((
+                        PbrBundle {
+                            mesh: shape.clone(),
+                            transform: Transform::from_xyz(x as f32, z as f32, y as f32),
+                            ..default()
+                        },
+                    ));
+                }
+            }
+        }
+    }
 }
