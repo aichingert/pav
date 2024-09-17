9bf298b749760fe92f58f31100a603d8d552c707
aichingert <aichinger.tobias13@gmail.com>
2024-09-17 20:07:12 +0200

    feat(window): able to create native window

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,18 @@
+use winit::{
+    event_loop::EventLoop,
+    window::{Window, WindowAttributes},
+};
+
+async fn run(ev: EventLoop<()>, window: Window) {
+    ev.run(move |_event, _target| {
+    }).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ev_loop = EventLoop::new().unwrap();
+    let window = ev_loop.create_window(WindowAttributes::default()).unwrap();
+
+    {
+        pollster::block_on(run(ev_loop, window));
+    }
 }
