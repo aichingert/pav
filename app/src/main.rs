c87b13f1f3b75981e58c7a4bd164e3b51a51f746
aichingert <aichinger.tobias13@gmail.com>
2024-09-24 23:32:38 +0200

    feat(wgpu): added basic state for a wgpu and redraw capabilities

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,96 @@
 use winit::{
+    event::{Event, WindowEvent},
     event_loop::EventLoop,
     window::{Window, WindowAttributes},
 };
 
+pub struct State<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+
+    window: &'a Window,
+}
+
+impl<'a> State<'a> {
+    pub async fn new(window: &'a Window) -> Self {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+        let adapter = instance.request_adapter({
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            }
+        }).await.unwrap();
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
+                label: None,
+                memory_hints: Default::default(),
+            },
+            None,
+        ).await.unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps.formats.iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size
+        }
+    }
+}
+
 async fn run(ev: EventLoop<()>, window: Window) {
-    ev.run(move |_event, _target| {
+    let mut state = State::new(&window).await;
+
+    ev.run(move |event, target| {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    target.exit();
+                },
+                WindowEvent::RedrawRequested => {
+                    state.window.request_redraw();
+                },
+                WindowEvent::Resized(physical_size) => {
+                    state.size = physical_size;
+                    state.config.width = physical_size.width;
+                    state.config.height = physical_size.height;
+                    state.surface.configure(&state.device, &state.config);
+                },
+                _ => (),
+            }
+            _ => (),
+        }
     }).unwrap();
 }
 
