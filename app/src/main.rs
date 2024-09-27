223894afc051122d8e1d78b63fb1af3b8b370d62
aichingert <aichinger.tobias13@gmail.com>
2024-09-27 22:19:51 +0200

    chore: figure out how to get do pointer arithmetic like the apple docs

--- app/src/main.rs
+++ app/src/main.rs
@@ -5,21 +5,16 @@ use winit::{
 };
 
 mod ttf;
-use ttf::parser::FontDirectory;
 mod state;
 
 mod app;
 use app::App;
 
 fn main() {
-
-    let file: Vec<u8> = std::fs::read("../tmp/envy/envy_code.ttf").unwrap();
-
-    let f = FontDirectory::read_section(&file);
-    println!("{f:?}");
-
     let ev_loop = EventLoop::new().unwrap();
     let window = ev_loop.create_window(WindowAttributes::default()).unwrap();
+
+    ttf::parser::parse("../tmp/envy/envy_code.ttf");
 
     pollster::block_on(App::run(ev_loop, &window));
 }
--- app/src/ttf/parser.rs
+++ app/src/ttf/parser.rs
@@ -1,12 +1,11 @@
-
 #[derive(Debug)]
-pub struct FontDirectory {
-    pub off_sub: OffsetSubtable,
-    tbl_dir: Vec<TableDirectory>,
+struct FontDirectory {
+    off_sub: OffsetSubtable,
+    tbl_dirs: Vec<TableDirectory>,
 }
 
 #[derive(Debug)]
-pub struct OffsetSubtable {
+struct OffsetSubtable {
     scaler_type: u32,
     num_tables: u16,
     search_range: u16,
@@ -22,17 +21,48 @@ struct TableDirectory {
     length: u32,
 }
 
-impl FontDirectory {
-    pub fn read_section(buf: &[u8]) -> Self {
-        let (off_sub, ptr) = OffsetSubtable::read_section(buf, 0);
-        let tbl_dir = TableDirectory::read_section(buf, ptr, off_sub.num_tables);
+#[derive(Debug)]
+struct CMap {
+    version: u16,
+    subtables_number: u16,
+    subtables: Vec<CMapEncodingSubtable>,
+}
 
-        Self { off_sub, tbl_dir }
+#[derive(Debug)]
+struct CMapEncodingSubtable {
+    platform_id: u16,
+    platform_specific_id: u16,
+    offset: u32,
+}
+
+#[derive(Debug)]
+struct Format4 {
+    format: u16,
+    length: u16,
+    language: u16,
+    seg_count_x2: u16,
+    search_range: u16,
+    entry_selector: u16,
+    range_shift: u16,
+    reserve_pad: u16,
+    end_code: Vec<u16>,
+    start_code: Vec<u16>,
+    id_delta: Vec<u16>,
+    id_range_offset: Vec<u16>,
+    glyph_id_array: Vec<u16>,
+}
+
+impl FontDirectory {
+    fn read(buf: &[u8]) -> (Self, usize) {
+        let (off_sub, ptr) = OffsetSubtable::read(buf, 0);
+        let (tbl_dirs, ptr) = TableDirectory::read(buf, ptr, off_sub.num_tables);
+
+        (Self { off_sub, tbl_dirs }, ptr)
     }
 }
 
 impl OffsetSubtable {
-    fn read_section(buf: &[u8], ptr: usize) -> (Self, usize) {
+    fn read(buf: &[u8], ptr: usize) -> (Self, usize) {
         (
             Self {
                 scaler_type:    u32_from_be_bytes(&buf[ptr + 0..ptr + 4]),
@@ -47,7 +77,7 @@ impl OffsetSubtable {
 }
 
 impl TableDirectory {
-    fn read_section(buf: &[u8], ptr: usize, tables: u16) -> Vec<Self> {
+    fn read(buf: &[u8], ptr: usize, tables: u16) -> (Vec<Self>, usize) {
         let tables = tables as usize;
         let mut tbl_dirs = Vec::with_capacity(tables);
 
@@ -62,7 +92,133 @@ impl TableDirectory {
             });
         }
 
-        tbl_dirs
+        (tbl_dirs, ptr + tables * 16)
+    }
+}
+
+impl CMap {
+    fn read(buf: &[u8], ptr: usize) -> (Self, usize) {
+        let version = u16_from_be_bytes(&buf[ptr + 0..ptr + 2]);
+        let subtables_number = u16_from_be_bytes(&buf[ptr + 2..ptr + 4]);
+        println!("n: {subtables_number}");
+        let mut subtables = Vec::with_capacity(subtables_number as usize);
+
+        for i in 0..subtables_number as usize {
+            let pos = ptr + 4 + i * 8;
+
+            subtables.push(CMapEncodingSubtable {
+                platform_id:            u16_from_be_bytes(&buf[pos + 0..pos + 2]),
+                platform_specific_id:   u16_from_be_bytes(&buf[pos + 2..pos + 4]),
+                offset:                 u32_from_be_bytes(&buf[pos + 4..pos + 8]),
+            });
+        }
+
+        (
+            Self {
+                version,
+                subtables_number,
+                subtables,
+            },
+            ptr + 4 + subtables_number as usize * 8,
+        )
+    }
+}
+
+impl Format4 {
+    fn read(buf: &[u8], ptr: usize) -> (Self, usize) {
+        let cap = u16_from_be_bytes(&buf[ptr + 6..ptr + 8]) as usize;
+
+        let mut fmt4 = Self {
+            format:         u16_from_be_bytes(&buf[ptr + 0..ptr + 2]),
+            length:         u16_from_be_bytes(&buf[ptr + 2..ptr + 4]),
+            language:       u16_from_be_bytes(&buf[ptr + 4..ptr + 6]),
+            seg_count_x2:   u16_from_be_bytes(&buf[ptr + 6..ptr + 8]),
+            search_range:   u16_from_be_bytes(&buf[ptr + 8..ptr +10]),
+            entry_selector: u16_from_be_bytes(&buf[ptr +10..ptr +12]),
+            range_shift:    u16_from_be_bytes(&buf[ptr +12..ptr +14]),
+            reserve_pad: 0,
+
+            end_code: Vec::with_capacity(cap),
+            start_code: Vec::with_capacity(cap),
+            id_delta: Vec::with_capacity(cap),
+            id_range_offset: Vec::with_capacity(cap),
+            glyph_id_array: Vec::with_capacity(cap),
+        };
+
+        let end_code_ptr = ptr + 14;
+        let start_code_ptr = ptr + 14 + fmt4.seg_count_x2 as usize + 2;
+        let id_delta_ptr = ptr + 14 + fmt4.seg_count_x2 as usize * 2 + 2;
+        let id_range_ptr = ptr + 14 + fmt4.seg_count_x2 as usize * 3 + 2;
+
+        let size = fmt4.seg_count_x2 as usize / 2;
+
+        assert_eq!(size, cap);
+
+        for i in 0..size {
+            fmt4.end_code.push(u16_from_be_bytes(&buf[end_code_ptr + i * 2..end_code_ptr + i * 2 + 2]));
+            fmt4.start_code.push(u16_from_be_bytes(&buf[start_code_ptr + i * 2..start_code_ptr + i * 2 + 2]));
+            fmt4.id_delta.push(u16_from_be_bytes(&buf[id_delta_ptr + i * 2..id_delta_ptr + i * 2]));
+            fmt4.id_range_offset.push(u16_from_be_bytes(&buf[id_range_ptr + i * 2..id_range_ptr + i * 2]));
+        }
+
+        let glyph_ptr = size * 8 + 2;
+        let remaining = fmt4.length as usize - (glyph_ptr - ptr);
+
+        for i in 0..remaining / 2 {
+            fmt4.glyph_id_array.push(
+                u16_from_be_bytes(&buf[glyph_ptr + i * 2..glyph_ptr + i * 2 + 2])
+            );
+        }
+
+        (fmt4, glyph_ptr + remaining)
+    }
+
+    // TODO
+    fn get_glyph_index(&self, code_point: u16) -> u16 {
+        let mut index = None::<usize>;
+
+        for (i, &end_code) in self.end_code.iter().enumerate() {
+            if end_code > code_point {
+                index = Some(i);
+                break;
+            }
+        }
+
+        let Some(ptr) = index else { return 0; };
+
+        if self.start_code[ptr] < code_point {
+            if self.id_range_offset[ptr] != 0 {
+                //let mut ptr = index as i32 + self.id_range_offset[index] as i32 / 2;
+                //ptr += code_point as i32 - self.start_code[index];
+                //println!("{ptr}");
+
+            } else {
+                return self.id_delta[ptr] + code_point;
+            }
+        }
+
+        0
+    }
+}
+
+pub fn parse(filename: &str) {
+    let file = std::fs::read("../tmp/envy/envy_code.ttf").unwrap();
+
+    let (fd, mut ptr) = FontDirectory::read(&file);
+    println!("{}", fd.tbl_dirs.len());
+
+    for tbl_dir in &fd.tbl_dirs {
+        if tbl_dir.tag == u32_from_be_bytes(b"cmap") {
+            let offset = tbl_dir.offset as usize + ptr;
+
+            let (cmap, nxt) = CMap::read(&file, offset);
+
+            for enc in &cmap.subtables {
+                println!("{enc:?}");
+            }
+
+            ptr = nxt;
+        }
     }
 }
 
